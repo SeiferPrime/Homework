ee961262556c72c5cea4ab577c0051bc2fd1b8ea
Seifer <seiferprime@gmail.com>
2015-08-10 22:41:13 +0300

    Ready for review

--- Exercise55.java
+++ Exercise55.java
@@ -1,67 +1,182 @@
 class Employee {
-	int[] INN = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	String[] Name = {"Vasya", "Masha", "Kolya", "Pasha", "Den4ik", "Dimon", "Vovan", "Kostyan", "Seva", "Lena"};
-	String[] Surname = {"Voshkin", "Malinovskaya", "Pashkovskii", "Nikolay4enko", "Kymov", "Vaev", "Ykyrov", "Tolstui", "Toshii", "Golova4"};
-	int[] Salary = {300, 250, 430, 125, 500, 700, 250, 100500, 100, 355};
+	private int INN;
+	private String Name;
+	private String SurName;
+	private double Salary;
+
+	public Employee() { }
+
+	public Employee(int a) {
+		setINN(a);
+	}
+
+	public Employee(int a, String b) {
+		this(a);
+		setName(b);
+	}
+
+	public Employee(int a, String b, String c) {
+		this(a, b);
+		setSurName(c);
+	}
+
+	public Employee(int a, String b, String c, double d) {
+		this(a, b, c);
+		setSalary(d);
+	}
+
+	public int getINN() {
+		return INN;
+	}
+
+	public void setINN(int INN) {
+			this.INN = INN;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+
+	public String getSurName() {
+		return SurName;
+	}
+
+	public void setSurName(String SurName) {
+		this.SurName = SurName;
+	}
+
+	public double getSalary() {
+		return Salary;
+	}
+
+	public void setSalary(double Salary) {
+		this.Salary = Salary;
+	}
+
+	public String toString() {
+		return "INN : " + getINN() + " Name : " + getName() + " Second name : " + getSurName() + " Salary : " + getSalary() + "$";
+	}
 
 }
 
 class Exercise55 {
 	public static void main(String[] args) {
-    Employee work = new Employee();
-    Report_a(work.INN, work.Name, work.Surname, work.Salary);
-    Report_b(work.Salary);
-    Report_c(work.INN, work.Name, work.Surname, work.Salary);
-    Report_d(work.INN, work.Name, work.Surname, work.Salary);
+		Employee p1 = new Employee(1, "Vasya", "Voshkin", 300);
+		Employee p2 = new Employee(2, "Masha", "Malinovskaya", 125);
+		Employee p3 = new Employee(3, "Kolya", "Pashkovskiy", 430);
+		Employee p4 = new Employee(4, "Pasha", "Nikolaychenko", 125);
+		Employee p5 = new Employee(5, "Denchik", "Kymov", 500);
+		Employee p6 = new Employee(6, "Dimon", "Vaev", 700);
+		Employee p7 = new Employee(7, "Vovan", "Ukurov", 250);
+		Employee p8 = new Employee(8, "Kostya", "Tolstui", 100500);
+		Employee p9 = new Employee(9, "Seva", "Toshiy", 310);
+		Employee p10 = new Employee(10, "Lena", "Golovach", 355);
+		Employee p11 = new Manager(11, "Stas", "Melnik", 500, 1000000);
+		Employee p12 = new Manager(12, "Kisa", "Vorobyaninov", 321);
+
+		Employee[] arr = new Employee[12];
+
+		arr[0] = p1;
+		arr[1] = p2;
+		arr[2] = p3;
+		arr[3] = p4;
+		arr[4] = p5;
+		arr[5] = p6;
+		arr[6] = p7;
+		arr[7] = p8;
+		arr[8] = p9;
+		arr[9] = p10;
+		arr[10] = p11;
+		arr[11] = p12;
+		
+		Report_a(arr);
+		Report_c(arr);
+		Report_d(arr);
+		Report_b(arr);
+		
 	}
 
-	static void Report_a(int[] INN, String[] Name, String[] Surname, int[] Salary) {
-		for (int i = 0; i < 10; i++) {
-			System.out.print(INN[i] + " ");
-			System.out.print(Name[i] + " ");
-			System.out.print(Surname[i] + " ");
-			System.out.print(Salary[i]);
-			System.out.println();
+	static void Report_a(Employee[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
 		}
+		System.out.println();
 	}
 
-	static void Report_b(int[] Salary) {
-		int result = 0;
-		for (int i = 0; i < 10; i++) {
-			result = result + Salary[i];
+	static void Report_b(Employee[] arr) {
+		double Sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			Sum += arr[i].getSalary(); 
 		}
-		System.out.println("Sum = " + result);
+		System.out.println("Total sum : " + Sum + "$");
+		System.out.println();
 	}
 
-	static void Report_c(int[] INN, String[] Name, String[] Surname, int[] Salary) {
-		int m = 0;
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				if (Salary[i] > Salary[j]) {
-					m = j;
-				}
+	static void Report_c(Employee[] arr) {
+		double min = arr[0].getSalary();
+		int n = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].getSalary() < min) {
+				n = i;
+				min = arr[i].getSalary();
 			}
-        }
-        System.out.print(INN[m] + " ");
-		System.out.print(Name[m] + " ");
-		System.out.print(Surname[m] + " ");
-		System.out.print(Salary[m]);
+		}
+		for (int i = 0; i < arr.length; i++) {
+		if (arr[n].getSalary() == arr[i].getSalary()) {
+			n = i;
+			System.out.println(arr[n]);
+			}
+		}
 		System.out.println();
-    }
+	}
 
-	static void Report_d(int[] INN, String[] Name, String[] Surname, int[] Salary) {
-		int m = 0;
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				if (Salary[i] < Salary[j]) {
-					m = j;
-				}
+	static void Report_d(Employee[] arr) {
+		double max = arr[0].getSalary();
+		int n = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].getSalary() > max) {
+				n = i;
+				max = arr[i].getSalary();
 			}
-        }
-        System.out.print(INN[m] + " ");
-		System.out.print(Name[m] + " ");
-		System.out.print(Surname[m] + " ");
-		System.out.print(Salary[m]);
+		}
+		for (int i = 0; i < arr.length; i++) {
+		if (arr[n].getSalary() == arr[i].getSalary()) {
+			n = i;
+			System.out.println(arr[n]);
+			}
+		}
 		System.out.println();
-    }    
+	}	
+
+}
+
+class Manager extends Employee {
+	private double bonus;
+
+	public Manager(int a, String b, String c, double d) {
+		super(a, b, c, d);
+		bonus = 0;
+	}
+
+	public Manager(int a, String b, String c, double d, double e) {
+		super(a, b, c, d);
+		setbonus(e);
+	}
+
+	public double getbonus() {
+		return bonus;
+	}
+
+	public void setbonus(double bonus) {
+		this.bonus = bonus;
+	}
+
+	public double getSalary() {
+		return super.getSalary() + getbonus();
+	}
+
 }
